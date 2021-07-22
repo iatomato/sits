f40dfffc222a54e54b6d602dd05c5a8dee3ef58f
iatomato <l_.ll@hotmail.com>
2021-07-22 08:43:43 +0800

    s_cmp

--- sits.h
+++ sits.h
@@ -13,16 +13,34 @@
  * Letters
  */
 
-typedef struct __m_obj m_cmp;
+/* C99 */
+#if defined(__linux__)
+#define OK  2048
+#define NK  0
 
-#define C_CMP(s0, s1)   ({ \
-    unsigned int lo_siz; \
-    typeof(lo_siz) index; \
-    typeof(lo_siz) s0_len = strlen(s0); \
-    typeof(lo_siz) s1_len = strlen(s1); \
-    lo_siz = ((s0_len < s1_len) ? s0_len : s1_len); \
-    for (; index < lo_siz; index++) { \
-    } \
-    })
+#include <stdint.h>
+
+static inline uint32_t s_cmp(char *s0, char *s1)
+{
+    uint32_t lo_size;
+    uint32_t index = 0;
+    uint32_t s0_len = strlen(s0);
+    uint32_t s1_len = strlen(s1);
+    lo_size = ((s0_len < s1_len) ? s0_len : s1_len);
+    for (; index < lo_size; index++) {
+        if (isdigit(*(s0 + index)) && isdigit(*(s1 + index))) {
+            return ((*(s0 + index) - '0') < (*(s1 + index) - '0') ? 0 : 1);
+        } else {
+            if (isdigit(*(s0 + index)) == OK) {
+                return 0;
+            } else if (isdigit(*(s1 + index)) == OK) {
+                return 1;
+            } else {
+                return ((int) *(s0 + index) < (int) *(s1 + index) ? 0 : 1);
+            }
+        }
+    }
+}
 
 #endif /* SITS_H_ */
+#endif /* __linux__ */
